3069fd05f5fb82259b0d8fcde9be52f040d35eff
Sethi Dhiyal <sethidhial906@gmail.com>
2025-09-19 16:24:46 +0530

    1st commit12345

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,86 +1,86 @@
 // ❌ fs/path remove kar diye (Vercel me persist nahi hota)
 
 // ✅ In-memory DB (Vercel-friendly)
-let users = [];
+// let users = [];
 
-/**
- * Get all users
- */
-export function getUsers() {
-  return users;
-}
-
-/**
- * Add a new user
- */
-export function addUser(user) {
-  users.push(user);
-}
-
-/**
- * Get user by email
- */
-export function getUserByEmail(email) {
-  return users.find((u) => u.email === email);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import fs from "fs";
-// import path from "path";
-
-// const dbPath = path.join(process.cwd(), "db.json");
-
-// function readDB() {
-//   const data = fs.readFileSync(dbPath, "utf-8");
-//   return JSON.parse(data);
-// }
-
-// function writeDB(data) {
-//   fs.writeFileSync(dbPath, JSON.stringify(data, null, 2));
-// }
-
+// /**
+//  * Get all users
+//  */
 // export function getUsers() {
-//   return readDB().users;
+//   return users;
 // }
 
+// /**
+//  * Add a new user
+//  */
 // export function addUser(user) {
-//   const db = readDB();
-//   db.users.push(user);
-//   writeDB(db);
+//   users.push(user);
 // }
 
+// /**
+//  * Get user by email
+//  */
 // export function getUserByEmail(email) {
-//   return getUsers().find((u) => u.email === email);
+//   return users.find((u) => u.email === email);
 // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import fs from "fs";
+import path from "path";
+
+const dbPath = path.join(process.cwd(), "db.json");
+
+function readDB() {
+  const data = fs.readFileSync(dbPath, "utf-8");
+  return JSON.parse(data);
+}
+
+function writeDB(data) {
+  fs.writeFileSync(dbPath, JSON.stringify(data, null, 2));
+}
+
+export function getUsers() {
+  return readDB().users;
+}
+
+export function addUser(user) {
+  const db = readDB();
+  db.users.push(user);
+  writeDB(db);
+}
+
+export function getUserByEmail(email) {
+  return getUsers().find((u) => u.email === email);
+}
 
 
 
